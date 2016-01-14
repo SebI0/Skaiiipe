838463f12326004103cb879ae92b3e4b710afcfb
Hyurt <elie.alawoe@gmail.com>
2016-01-14 17:49:45 +0100

    Commentaires de Connexion Serveur InfoServeur et début Serveur

--- Skaiiipe/src/server/Server.java
+++ Skaiiipe/src/server/Server.java
@@ -5,18 +5,14 @@
  */
 package server;
 
-import java.awt.Color;
 import java.io.IOException;
 import java.io.Serializable;
-import java.net.BindException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JList;
 import javax.swing.JTree;
@@ -26,54 +22,85 @@ import modele.Salon;
 import services.InfosServeur;
 
 /**
+ * Classe permettant la création d'un serveur principal
  *
  * @author Elie
  */
 public class Server extends Thread implements Serializable {
 
+    /**
+     * @param listServer Objet du type InfosServeur permetant d'avoir des
+     * informations d'un salon
+     * @see InfosServeur
+     * @param salons Liste de salon permetant de lister les salons
+     * @see Salon
+     * @param id Identifiant unique du serveur principal
+     * @param ip Adresse Inet utilisé par le serveur
+     * @param port Entier contentant le port d'écoute du salon
+     * @param treeSalons Arbre contenant les salons pour l'affichage
+     * @param listUser Liste d'utilisateur servant à l'affichage des
+     * utilisateurs d'un salon
+     * @param incre Valeur numérique servant à l'identification unique d'un
+     * serveur
+     */
     InfosServeur listServers;
     private ArrayList<Salon> salons;
     int id;
-
     private InetAddress ip; //ip utilisée par le serveur de salons
     private int port; //port utilisé par le serveur de salons
     private JTree treeSalons; //affichage de la liste des salons
     private JList listUsers;
-
     public static int incre = 1000;
 
+    /**
+     * Constructeur par défaut d'un serveur
+     */
     public Server() {
         id = incre;
         incre++;
     }
 
+    /**
+     * Consctructeur d'un serveur
+     *
+     * @param ip AdresseInet du serveur
+     * @param port Port d'écoute du salon
+     * @param treeSalons Arbre contenant les salons pour l'affichage des
+     * utilisateurs du salon
+     * @param listUsers Liste d'utilisateur servant à l'affichage des
+     * utilisateurs d'un salon
+     */
     public Server(InetAddress ip, int port, JTree treeSalons, JList listUsers) {
-        this.ip = ip;
-        this.port = port;
-        this.treeSalons = treeSalons;
-        this.listUsers = listUsers;
+        Server s = new Server();
+        s.ip = ip;
+        s.port = port;
+        s.treeSalons = treeSalons;
+        s.listUsers = listUsers;
     }
 
+    /**
+     * Réécriture du processus serveur
+     */
     @Override
     public void run() {
+        /**
+         * Initialisation des variables de listing
+         */
         listServers = new InfosServeur();
         this.salons = new ArrayList<Salon>();
 
         try {
+            //Initialisation socket d'écoute principale
             ServerSocket s2 = new ServerSocket();
 
             InetSocketAddress sa = new InetSocketAddress(this.ip, this.port);
             s2.bind(sa);
-            System.out.println("__serveur__ Serveur de salons crée !");
-            System.out.println("__serveur__ IP : " + InetAddress.getLocalHost() + " port : " + this.port);
-
+            
             updateListeSalons();
             while (!Thread.currentThread().isInterrupted()) {
+                //Acceptation des connexions
                 Socket s = s2.accept();
                 ConnexionServeur ServerConnexion = new ConnexionServeur(s, this);
-                System.out.println("__serveur__ Server " + id + ": Ajout d'une connexion Serveur");
-                //listServers.add(ServerConnexion.getInfos());
-                System.out.println("__serveur__ Il y a actuellement: " + listServers.size() + " serveurs en ligne");
                 ServerConnexion.start();
             }
         } catch (IOException ex) {
@@ -86,7 +113,6 @@ public class Server extends Thread implements Serializable {
         for (Salon elt : listServers.get()) {
             System.out.println(elt.getNom() + ":" + elt.active);
             if (elt.active) {
-
                 SalonActifs.add(elt);
             }
         }
