9026a2af004f28e03976e6e28c39bcc59baf2de8
Hyurt <elie.alawoe@gmail.com>
2016-01-14 17:11:21 +0100

    Commentaires de Broadcaster et ConnexionClient

--- Skaiiipe/src/server/Broadcaster.java
+++ Skaiiipe/src/server/Broadcaster.java
@@ -17,54 +17,102 @@ import tppaint2014.Forme;
 import vue.Host;
 
 /**
+ * Classe du thread permetant d'écouter les messages du client vers l'hôte et de
+ * les diffuser par la suite
  *
  * @author Elie
  */
 public class Broadcaster extends Thread {
 
+    /**
+     * @param socketServer Socket liant le client et le serveur
+     * @param hote Lien vers le processus père de l'hôte
+     * @see Host
+     * @param InputClient Flux de lecture des données du client vers l'hôte
+     * @param InputClient Flux d'écriture des données du client vers l'hôte
+     */
     private Socket socketServer;
     private Host hote;
-    private List<Broadcaster> ListClient;
     private ObjectInputStream InputClient;
     private ObjectOutputStream OutputClient;
 
+    /**
+     * Constructeur d'un processus Broadcaster
+     *
+     * @param socketServer liant le client et le serveur
+     * @param hote Lien vers le processus père de l'hôte
+     */
     public Broadcaster(Socket socketServer, Host hote) {
         this.socketServer = socketServer;
         this.hote = hote;
     }
 
+    /**
+     * Retourne la socket entre hôte/client
+     *
+     * @return socketServer Socket entre client/serveur
+     */
     public Socket getSocket() {
         return socketServer;
     }
 
+    /**
+     * Assigner une valeur de socket entre hôte/client
+     *
+     * @param socketServer Socket entre client/serveur
+     */
     public void setSocket(Socket socketServer) {
         this.socketServer = socketServer;
     }
 
+    /**
+     * Retourne le processus Hote
+     *
+     * @return hote
+     * @see Host
+     */
     public Host getHote() {
         return hote;
     }
 
+    /**
+     * Assigner a un processus un processus hôte
+     *
+     * @param hote
+     * @see Host
+     */
     public void setHote(Host hote) {
         this.hote = hote;
     }
 
-    public List<Broadcaster> getListClient() {
-        return ListClient;
-    }
-
-    public void setListClient(List<Broadcaster> ListClient) {
-        this.ListClient = ListClient;
-    }
-
+    /**
+     * Retourne le flux de lecture de données
+     *
+     * @return InputClient Flux de lecture des données du client vers l'hôte
+     */
     public ObjectInputStream getInputClient() {
         return InputClient;
     }
 
+    /**
+     * Retourne le flux d'écriture de données
+     *
+     * @param InputClient Flux d'écriture des données du client vers l'hôte
+     */
     public ObjectOutputStream getOutputClient() {
         return OutputClient;
     }
 
+    /**
+     * Permet de diffuser un message à tout les utilisateurs connectés à un
+     * salon.
+     *
+     * @param c Broadcast permetant d'identifer l'émetteur du message afin de ne
+     * pas le lui renvoyer l'information
+     * @param m Message que l'on souhaite diffuser
+     * @see Message
+     * @throws IOException
+     */
     public void broadcastAllUser(Broadcaster c, Message m) throws IOException {
         System.out.println("HelloBroadcast");
         int incre = 0;
@@ -78,12 +126,20 @@ public class Broadcaster extends Thread {
 
     }
 
+    /**
+     * Définition du thread qui est executé coté serveur pour chaque client qui
+     * se connecte. Chaque client possède son thread qui permet au serveur
+     * d'écouter le client. Lors de son execution, on initialiser les différents
+     * flux d'entrée/sortie et nous renvoyons à l'utilisateur les formes déjà
+     * tracées
+     */
     @Override
     public void run() {
         try {
-            //écouteur du client maitre
+            //Initialisation écouteur du client maitre
             InputClient = new ObjectInputStream(socketServer.getInputStream());
             OutputClient = new ObjectOutputStream(socketServer.getOutputStream());
+            //Renvoie des formes que l'on va chercher dans la fenêtre de l'hôte
             for (Forme fe : hote.getFen().lesFormes) {
                 OutputClient.writeObject(new Message(Message.FORME, fe));
             }
@@ -91,14 +147,18 @@ public class Broadcaster extends Thread {
             Logger.getLogger(ConnexionClient.class.getName()).log(Level.SEVERE, null, ex);
         }
 
+        //Écoute du réseau
         while (!Thread.currentThread().isInterrupted()) {
             try {
+                //En attente d'un message
                 Message msg = (Message) InputClient.readObject();
-                
-                    broadcastAllUser(this, msg);
-                if(msg.getType()==Message.FERMETURE_SALON)
-                      hote.setActive(false);
-
+                //Diffusion du message
+                broadcastAllUser(this, msg);
+                //Si le message est un message de fermeture de salon, on désactive l'hote.
+                if (msg.getType() == Message.FERMETURE_SALON) {
+                    hote.setActive(false);
+                }
+            //Gestion des erreurs
             } catch (IOException ex) {
                 Logger.getLogger(ConnexionClient.class.getName()).log(Level.SEVERE, null, ex);
             } catch (ClassNotFoundException ex) {
--- Skaiiipe/src/server/ConnexionClient.java
+++ Skaiiipe/src/server/ConnexionClient.java
@@ -19,45 +19,77 @@ import tppaint2014.Forme;
 import vue.Host;
 
 /**
+ * Processus coté client en écoute de l'hote pour receptionner ses messages
  *
  * @author Elie
  */
 public class ConnexionClient extends Thread {
 
+    /**
+     * @param socketServer Socket de dialogue avec l'hôte
+     * @param id Identifiant numérique du salon
+     * @param InputClient Flux de lecture des données du client vers l'hôte
+     * @param fen Lien d'accès à la fenetre du client.
+     * @param username Nom d'utilisateur du client.
+     */
     Socket socketServer;
     int id;
     public static int incre = 0;
     private ObjectInputStream InputClient;
     private Fenetre fen;
-    private Host hote;
     private String username;
 
+    /**
+     * Constructeur du thread
+     *
+     * @param socketServer Socket entre hote et client
+     * @param f Fenetre de l'utilisateur
+     * @throws IOException
+     */
+    public ConnexionClient(java.net.Socket socketServer, Fenetre f) throws IOException {
+        this.fen = f;
+        this.socketServer = socketServer;
+        this.id = incre;
+        this.incre++;
+        this.username = username;
+    }
+
+    /**
+     * Retourne le flux de lecture de données
+     *
+     * @return InputClient Flux de lecture des données du client vers l'hôte
+     */
     public ObjectInputStream getInputClient() {
         return InputClient;
     }
 
+    /**
+     * Retourne la socket entre hôte/client
+     *
+     * @return socketServer Socket entre client/serveur
+     */
     public Socket getSocket() {
 
         return socketServer;
 
     }
 
+    /**
+     * Permet d'assigner a ce thread la fenetre de l'utilisateur
+     *
+     * @param f Fenetre de l'utilisateur
+     */
     public void SetFenetre(Fenetre f) {
         this.fen = f;
         System.out.println("Fenetre initialisée");
 
     }
 
-    public ConnexionClient(java.net.Socket socketServer, Fenetre f) throws IOException {
-        //  this.hote = hote;
-        this.fen = f;
-        this.socketServer = socketServer;
-        this.id = incre;
-        this.incre++;
-        this.username = username;
-
-    }
-
+    /**
+     * Définition du thread qui est executé coté client lors de la connexion à
+     * un hote. Ce thread est à l'écoute de l'hôte lorsqu'il souhaite lui
+     * envoyer un message et l'interprète
+     */
     @Override
     public void run() {
 
@@ -71,9 +103,12 @@ public class ConnexionClient extends Thread {
 
         while (!Thread.currentThread().isInterrupted()) {
             try {
+                //En attente d'un message
                 Message msg = (Message) InputClient.readObject();
                 Forme receivedForme;
+                //Interprétation du message
                 switch (msg.getType()) {
+                    //Si le message est une forme il faut l'afficher
                     case Message.FORME:
                         receivedForme = (Forme) msg.getData();
                         System.out.println(fen);
@@ -82,10 +117,6 @@ public class ConnexionClient extends Thread {
 
                         System.out.println("Une forme est reçue");
                         break;
-                    case Message.FERMETURE_SALON:
-                        JOptionPane.showMessageDialog(null, "L'hôte s'est déconnecté");
-                        fen.dispose();
-                        break;
 
                     case Message.GOMME:
                         receivedForme = (Forme) msg.getData();
@@ -93,6 +124,16 @@ public class ConnexionClient extends Thread {
                         fen.zg.repaint();
                         System.out.println(receivedForme);
                         break;
+
+                    //Si le message est une annonce de fermeture du salon, on prévient
+                    //l'utilisateur de sa déconnexion
+                    case Message.FERMETURE_SALON:
+                        JOptionPane.showMessageDialog(null, "L'hôte s'est déconnecté");
+                        fen.dispose();
+                        break;
+                    
+                        
+                    //Diverses action lié à l'affichage du salon
                     case Message.EFFACER:
                         fen.bh.EffacerLast(false);
                         break;
