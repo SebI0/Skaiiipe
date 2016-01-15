25e1e301fa6591e6d231c15a5681a510873614d6
Hyurt <elie.alawoe@gmail.com>
2016-01-15 10:52:07 +0100

    Fix v2

--- Skaiiipe/src/server/InfoServeur.java
+++ Skaiiipe/src/server/InfoServeur.java
@@ -8,37 +8,21 @@ package server;
 import java.io.Serializable;
 
 /**
- * Objet permettant de communiquer des informations au serveurs/client
+ *
  * @author Elie
  */
 public class InfoServeur implements Serializable {
 
-    /**
-     * @param adresse String adresse du serveur
-     * @param port Port d'écoute du serveur
-     * @param nom Nom du serveur
-     */
     String adresse;
     int port;
     String nom;
 
-    
-    /**
-     * Constructeur de l'objet
-     * @param adresse String adresse du serveur
-     * @param port Port d'écoute du serveur
-     * @param nom Nom du serveur
-     */
     public InfoServeur(String adresse, int port, String nom) {
         this.adresse = adresse;
         this.port = port;
         this.nom = nom;
     }
 
-    /**
-     * Réécriture de la fonction toString pour la mise en forme
-     * @return Retourne une chaine de caractères avec les informations de l'objet
-     */
     @Override
     public String toString() {
         return "Serveur : " + nom + "adresse= " + adresse + ", port=" + port;
